48e04813d7da041737593c943c07c07059a5cf39
aravindhvetri <2002aravinthv@gmail.com>
2025-12-04 17:33:03 +0530

    aari's initial push to main 04/12/2025 05:32 pm

---
+++ src/webparts/strangerThingsIntranet/components/IStrangerThingsIntranetProps.ts
@@ -0,0 +1,7 @@
+export interface IStrangerThingsIntranetProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/strangerThingsIntranet/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IStrangerThingsIntranetWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'StrangerThingsIntranetWebPartStrings' {
+  const strings: IStrangerThingsIntranetWebPartStrings;
+  export = strings;
+}
